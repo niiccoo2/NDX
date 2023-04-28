81bc6c42e5bfe03bcdb2b928e024298cf9c13297
niiccoo2 <101956935+niiccoo2@users.noreply.github.com>
2023-04-28 15:56:22 -0400

    Create rngg.py

---
+++ rngg.py
@@ -0,0 +1,11 @@
+def RNGG():
+  import random
+  RN=random.randint(1,100)
+  guess=print("type a number from one to one hundred.")
+  while RN not == guess
+  if RN == guess:
+    print("You got the number!")
+  elif RN >= guess:
+    print("Lower.")
+  elif RN <= guess:
+    print ("Higher.")
