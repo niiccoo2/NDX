9dbeb391c3e8423f2d1e44393506c81263e1a138
niiccoo2 <101956935+niiccoo2@users.noreply.github.com>
2023-04-28 15:44:25 -0400

    Update calc.py

--- calc.py
+++ calc.py
@@ -1,4 +1,5 @@
 def calc():
+  import math
   RED = '\033[91m'
   GREEN = '\033[92m'
   YELLOW = '\033[93m'
@@ -8,16 +9,18 @@ def calc():
   RESET = '\033[0m'
   while True:
     number1=input(GREEN + "What is the first number?\n" + RESET)
-    operation=input(YELLOW + "What operation are you using? (A, S, M, D, E, F, or q to quit)\n" + RESET)
+    operation=input(YELLOW + "What operation are you using? (A, S, M, D, E, F, MAN or q to quit)\n" + RESET)
     operation=operation.upper()
     number1=int(number1)
-    number2=int(number2)
     if operation=="Q":
       break
     elif operation=="F":
-      print(number1!)
+      print(math.factorial(number1))
+    elif operation=="MAN":
+      print("TEST")  
     number2=input(GREEN + "What is your second number?\n" + RESET)
     print("")
+    number2=int(number2)
     if operation=="A":
       print(number1+number2)
     elif operation=="S":
